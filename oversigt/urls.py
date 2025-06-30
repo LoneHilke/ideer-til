0a22f274f3cc884adc3c162e5d5e9076fe97270a
LMH <90649822+LoneHilke@users.noreply.github.com>
2025-06-30 21:29:02 +0200

    Alt igennem
    vil prøve at deploy i fly

--- oversigt/urls.py
+++ oversigt/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
-from .views import Forside,Info
+from .views import Forside,Info,Fane, Side, Knap, Ikon, Tekst, Tab, Rul
 
 urlpatterns = [
   path('', Forside.as_view(), name='forside'),
   path('info', Info.as_view(), name='info'),
+  path('fane', Fane.as_view(), name='fane'),
+  path('side', Side.as_view(), name='side'),
+  path('knap', Knap.as_view(), name='knap'),
+  path('ikon', Ikon.as_view(), name='ikon'),
+  path('tekst', Tekst.as_view(), name='tekst'),
+  path('tab', Tab.as_view(), name='tab'),
+  path('rul', Rul.as_view(), name='rul'),
   
 ] 
--- oversigt/views.py
+++ oversigt/views.py
@@ -8,3 +8,31 @@ class Forside(View):
 class Info(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'pversigt/info.html')
+    
+class Fane(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pversigt/fane.html')
+    
+class Tab(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pversigt/tap.html')
+    
+class Rul(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pversigt/rul.html')
+    
+class Ikon(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pversigt/ikon.html')
+    
+class Tekst(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pversigt/tekst.html')
+    
+class Side(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pversigt/side.html')
+    
+class Knap(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pversigt/Knapper.html')
