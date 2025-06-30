37225dfe72ff369de9a22ba7dcdb89ffa380831b
LMH <90649822+LoneHilke@users.noreply.github.com>
2025-06-30 13:41:28 +0200

    ekstra faner
    sat ekstra faner ind og startet på dem

--- oversigt/urls.py
+++ oversigt/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import Forside
+from .views import Forside,Info
 
 urlpatterns = [
   path('', Forside.as_view(), name='forside'),
+  path('info', Info.as_view(), name='info'),
   
 ] 
--- oversigt/views.py
+++ oversigt/views.py
@@ -4,3 +4,7 @@ from django.views import View
 class Forside(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'pversigt/forside.html')
+    
+class Info(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'pversigt/info.html')
